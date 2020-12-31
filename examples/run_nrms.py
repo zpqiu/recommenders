29cb4ad7f16624fdfaa68780ce105affde39690a
zhaopengqiu <zhaopengqiu@tencent.com>
2020-12-31 17:30:57 +0800

    add nrms running script

--- examples/run_nrms.py
+++ examples/run_nrms.py
@@ -12,30 +12,30 @@ from reco_utils.recommender.newsrec.newsrec_utils import prepare_hparams
 from reco_utils.recommender.newsrec.models.nrms import NRMSModel
 from reco_utils.recommender.newsrec.io.mind_iterator import MINDIterator
 from reco_utils.recommender.newsrec.newsrec_utils import get_mind_data_set
-from tempfile import TemporaryDirectory
-import tensorflow as tf
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+
 
 print("System version: {}".format(sys.version))
 print("Tensorflow version: {}".format(tf.__version__))
-
-# tmpdir = TemporaryDirectory()
-# print(tmpdir)
 
 epochs = 10
 seed = 42
 MIND_type = 'large'
 
-data_path = "./test_nrms"
+data_path = "./test_mind"
 
 train_news_file = os.path.join(data_path, 'train', r'news.tsv')
 train_behaviors_file = os.path.join(data_path, 'train', r'behaviors.tsv')
 valid_news_file = os.path.join(data_path, 'valid', r'news.tsv')
-valid_behaviors_file = os.path.join(data_path, 'valid', r'behaviors.tsv')
+fast_valid_behaviors_file = os.path.join(data_path, 'valid', r'behaviors.small.tsv')
 wordEmb_file = os.path.join(data_path, "utils", "embedding.npy")
 userDict_file = os.path.join(data_path, "utils", "uid2index.pkl")
 wordDict_file = os.path.join(data_path, "utils", "word_dict.pkl")
-yaml_file = os.path.join(data_path, "utils", r'nrms.yaml')
+yaml_file = os.path.join(data_path, "utils", r'npa.yaml')
+model_dir = os.path.join(data_path, "nrms")
 
 mind_url, mind_train_dataset, mind_dev_dataset, mind_utils = get_mind_data_set(MIND_type)
 
@@ -53,15 +53,15 @@ hparams = prepare_hparams(yaml_file, wordEmb_file=wordEmb_file, \
                           wordDict_file=wordDict_file, userDict_file=userDict_file, \
                           epochs=epochs,
                           show_step=10)
-print(hparams)
+print("[NRMS] Config,", hparams)
 
 iterator = MINDIterator
 model = NRMSModel(hparams, iterator, seed=seed)
 
-print(model.run_eval(valid_news_file, valid_behaviors_file))
+print("[NRMS] First run:", model.run_eval(valid_news_file, fast_valid_behaviors_file))
 
-model.fit(train_news_file, train_behaviors_file, valid_news_file, valid_behaviors_file)
+model.fit(train_news_file, train_behaviors_file, valid_news_file, fast_valid_behaviors_file, model_save_path=model_dir)
 
-res_syn = model.run_eval(valid_news_file, valid_behaviors_file)
-print(res_syn)
+# res_syn = model.run_eval(valid_news_file, valid_behaviors_file)
+# print(res_syn)
 
